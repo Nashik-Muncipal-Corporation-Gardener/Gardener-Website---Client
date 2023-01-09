275fe9e7ac26f404d072bdf06a697b769610a614
Shraddha Mulay <76682198+shraddhamulay09@users.noreply.github.com>
2023-01-09 23:04:59 +0530

    navbar created

--- gardener/src/Components/NavigationBar.js
+++ gardener/src/Components/NavigationBar.js
@@ -1,15 +1,39 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import "bootstrap/dist/css/bootstrap.css";
+import {Button, Container, Navbar, NavDropdown, Offcanvas, Nav, Image} from 'react-bootstrap';
+import '../css/header.css';
 const NavigationBar = () => {
   return  (
-    <div className='nav'>
-      <div className='right-nav'>
-        <a className='navlink' href="url">Home</a>
-        <a className='navlink' href="url">About Us</a>
-        <a className='navlink' href="url">Garden</a>
-        <a className='navlink' href="url">Contact Us</a>
-        <Button className='signinbtn' variant="success">Sign In | Sign Up</Button>
-      </div>
+    <div className="Nav_bar">
+      {['sm'].map((expand) => (
+        <Navbar key={expand} expand={expand} className="mb-3">
+          <Container fluid>
+            <Navbar.Brand href="#"><img className="logo" src = {require ('../images/logo.png')}/></Navbar.Brand>
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link href="#action1">Home</Nav.Link>
+                  <Nav.Link href="#action2">About Us</Nav.Link>
+                  <Nav.Link href="#action1">Gardens</Nav.Link>
+                  <Nav.Link href="#action2">Contact Us</Nav.Link> 
+                  <Nav.Link className='signin_signup'><a><span style={{marginLeft:20}}></span>Sign In </a><span>|</span><a> Sign Up<span style={{marginRight:20}}></span></a></Nav.Link>
+                </Nav>
+                
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      ))}
     </div>
   );
 }
