764e6f8e588c8408ca616d400795cc4963196303
Shraddha Mulay <76682198+shraddhamulay09@users.noreply.github.com>
2023-01-10 00:22:15 +0530

    footer created

--- gardener/src/App.js
+++ gardener/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/NavigationBar';
+import Footer from './Components/footer'
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
