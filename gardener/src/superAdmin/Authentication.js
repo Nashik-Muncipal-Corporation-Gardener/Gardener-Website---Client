d6ebefe48a954e87afe47c4d20ce57f906461ca4
ShivamVerma380 <shivam.r.verma@seagate.com>
2023-01-10 20:06:09 +0530

    Authentication page with conditions added for super admin
    Signed-off-by: ShivamVerma380 <shivam.r.verma@seagate.com>

--- gardener/src/superAdmin/Authentication.js
+++ gardener/src/superAdmin/Authentication.js
@@ -1,12 +1,50 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Navbar from '../Components/NavigationBar';
-import {Container,Row, Col, Form, Button } from 'react-bootstrap';
+import {Container,Row, Col, Form, Button,Toast } from 'react-bootstrap';
 import '../css/signin.css'
+// import {toast,ToastContainer} from "react-toastify"
 
 function Authentication(){
 
     const [signIn, setSignIn] = React.useState(true);
+
+    const [email,setEmail] = React.useState('');
+    const [password,setPassword] = React.useState('');
+    const [confirmPassword,setConfirmPassword] = React.useState('');
+    const [name,setName] = React.useState('');
+    const [contact,setContact] = React.useState('');
+    const [state,setState] = React.useState('');
+
+    const handleEmailInput=(event)=>{
+        // console.log("Event"+event.target.value)
+        setEmail(event.target.value)
+        // email = event.target.value
+    }
+
+    const handlePasswordInput = (event)=>{
+        setPassword(event.target.value)
+    }
+
+    const handlePassword = (event)=>{
+        setPassword(event.target.value)
+    }
+
+    const handleConfirmmPassword = (event)=>{
+        setConfirmPassword(event.target.value);
+    }
+
+    const handleName = (event)=>{
+        setName(event.target.value);
+    }
+
+    const handleContact = (event)=>{
+        setContact(event.target.value);
+    }
+
+    const handleState = (event)=>{
+        setState(event.target.value);
+    }
 
 
     function openSignUp(){
@@ -17,11 +55,44 @@ function Authentication(){
         setSignIn(true)
     }
 
+    function registerSuperAdmin(){
+        // alert('Registering S
+        if (email === "") {
+            alert("Email is empty");
+            // console.log("Email is empty");
+            // toast.error(<b>Please enter email</b>)
+            return;
+        }else if(password.length<6){
+            alert("Password must be atleast of 6 characters")
+        }else if(password!==confirmPassword){
+            alert("Password and confirm password must be same")
+        }else if(name===""){
+            alert("Name is empty")
+        }else if(contact.length!=10 ){
+            alert("Please enter 10 digit mobile number")
+        }else{
+            alert("Registration successfull")
+        }
+    }
+
+    function handleLogin(){
+        
+        if(email===""){
+            alert("Email is empty")
+        }else if(password===""){
+            alert("Password is empty")
+        }else{
+            alert("Login successfull")
+        }
+
+    }
+
     return(
         <div>
             {
             (signIn==true)?(
                     <div className='signin'>
+                        {/* <ToastContainer position="top-center"/> */}
                         <Navbar/>
                         <Container>
                             <Row><Col><h4>Sign In</h4></Col></Row>
@@ -29,14 +100,20 @@ function Authentication(){
                                 <Col>
                                 <Form>
                                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                                        <Form.Control type="email" placeholder="Enter email" />
+                                        <Form.Control id="email" type="email" placeholder="Enter email" onChange={handleEmailInput} />
                                     </Form.Group>
 
                                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                                        <Form.Control type="password" placeholder="Password" />
+                                        <Form.Control id="password"  type="password" placeholder="Password" onChange={handlePassword}/>
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="formBasicPassword">
                                         <h7>Forgot password ?</h7>
+                                    </Form.Group>
+
+                                    <Form.Group style={{display:'flex', justifyContent:'center'}}>
+                                        <Button className='btn_signup' variant="outline-primary" type="submit" onClick={handleLogin}>
+                                            Login
+                                        </Button>
                                     </Form.Group>
                                     
                                     <Form.Group style={{display:'flex', justifyContent:'center'}}>
@@ -54,6 +131,7 @@ function Authentication(){
                     </div>
             ):(
                 <div className='signup'>
+                
                 <Navbar/>
                 <Container>
                     <Row><Col><h4>Sign Up</h4></Col></Row>
@@ -61,21 +139,21 @@ function Authentication(){
                         <Col>
                         <Form>
                             <Form.Group>
-                                <Form.Control type="email" placeholder="Email" />
-                                <Form.Control type="text" placeholder="Name" />
-                                <Row>
-                                    <Col md={2}><Button className='DOB'style={{marginBottom:'0 !important'}}>DOB</Button></Col>
-                                    <Col md={10}><Form.Control className='date_input' type="date" placeholder=" Date of Birth" /></Col>
-                                </Row>
-                                <Form.Control type="number" placeholder="Contact No.    " />
-                                <Form.Control type="text" placeholder="Street Address" />
-                                <Form.Control type="text" placeholder="City" />
-                                <Form.Control type="text" placeholder="State" />
-                                <Form.Control type="password" placeholder="Password" />
-                                <Form.Control type="password" placeholder="Confirm Password" />
+                                <Form.Control type="email" placeholder="Email" onChange={handleEmailInput} />
+                                <Form.Control type="password" placeholder="Password" onChange={handlePassword}/>
+                                <Form.Control type="password" placeholder="Confirm Password" onChange={handleConfirmmPassword}/>
+                                <Form.Control type="text" placeholder="Name" onChange={handleName}/>
+                                <Form.Control type="text" placeholder="Contact No." onChange={handleContact}/>
+                                <Form.Control type="text" placeholder="City"  value="Nashik"/>
+                                <Form.Control type="text" placeholder="State"  value="Maharashtra"/>
 
                             </Form.Group>
                             <br/>
+                            <Form.Group style={{display:'flex', justifyContent:'center'}}>
+                                <Button className='btn_signup' variant="outline-primary" type="submit" onClick={registerSuperAdmin}>
+                                    Register
+                                </Button>
+                            </Form.Group>
                             <Form.Group style={{display:'flex', justifyContent:'center'}}>
                                 <Button className='btn_signup' variant="outline-primary" type="submit" onClick={openSignIn}>
                                     Sign In
