f5cb066570280e617398200171621cd524e68a11
ShivamVerma380 <shivam.r.verma@seagate.com>
2023-01-22 08:43:44 +0530

    Added upload garden details and jogging track ui on super admin
    Signed-off-by: ShivamVerma380 <shivam.r.verma@seagate.com>

--- gardener/src/Uri.js
+++ gardener/src/Uri.js
@@ -1,4 +1,4 @@
 
-var url = "http://52.66.202.167:8080";
-
+// var url = "http://52.66.202.167:8080";
+var url = "http://localhost:8080"
 export default url;
--- gardener/src/superAdmin/Addgardens.js
+++ gardener/src/superAdmin/Addgardens.js
@@ -8,28 +8,89 @@ import React, { useState } from "react";
 import { Button, Card, Container, DropdownButton, ListGroup} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import SuperAdminHome from '../superAdmin/home/SuperAdminHome'
+
+import axios from 'axios'
+import url from '../Uri';
+
+
+
+
+
 function Addgardens() {
+
+  const navigate = useNavigate()
+
+  function handleGardenFilesUpload(event) {
+    event.preventDefault();
+    const gardensFile = document.getElementById('gardensFile').files[0]
+    console.log(gardensFile)
+  
+    var form_data = new FormData();
+
+    form_data.append('file', gardensFile);
+
+    axios.post(url+"/excel/gardens",form_data,{
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Authorization": "Bearer " + localStorage.getItem("jwtTokenSuperAdmin")
+      }
+    }).then(function(response){
+      if(response.status==200){
+        alert("Gardens Uploaded Successfully")
+      }else{
+        console.log("Error in uploading gardens",response.data)
+      }
+    }).catch(function(error){
+      console.log(error)
+    })
+  }
+
+
+  function handleJoggingTrackFilesUpload(event){
+    event.preventDefault();
+    const joggingTrackFile = document.getElementById('joggingTrackFile').files[0]
+
+    console.log(joggingTrackFile)
+
+    var form_data = new FormData();
+
+    form_data.append('file', joggingTrackFile);
+
+    axios.post(url+"/excel/joggingTrack",form_data,{
+      headers: {
+        "Content-Type": "multipart/form-data",
+        "Authorization": "Bearer " + localStorage.getItem("jwtTokenSuperAdmin")
+      }
+    }).then(function(response){
+      if(response.status==200){
+        alert("Jogging Track Uploaded Successfully")
+      }else{
+        console.log("Error in uploading gardens",response.data)
+      }
+    }).catch(function(error){
+      console.log(error)
+    })
+
+  }
+
+
   return (
         <div className='signin' style={{padding:'2%'}}>
         <div className='addgarden_container'>
-        <h1 style={{fontSize:30}}>Add Garden</h1>
+        <h1 style={{fontSize:30}}>Garden</h1>
         <form >
-          <MDBFile style={{marginBottom:20}} label='Add Image' id='customFile' />
-          <MDBFile style={{marginBottom:20}} label='Add Image' id='customFile' />
-          <MDBFile style={{marginBottom:20}} label='Add Image' id='customFile' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Name' />
-          <MDBInput className='mb-4' type='number' id='form2Example1' placeholder ='Garden Area' />
-          <MDBInput className='mb-4' type='number' id='form2Example1' placeholder ='Garden Establishment Year' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Description' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Facilities' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Uniqueness' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Opening Time' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Closing Time' />
-          <MDBInput className='mb-4' type='Text' id='form2Example1' placeholder ='Garden Location (add google map link)' />
+          <MDBFile style={{marginBottom:20}} label='Upload Gardens File' id='gardensFile' />
 
-          <Button type='submit' className='mb-4' block>
-            Submit
+          <Button type='submit' className='mb-4' block onClick={handleGardenFilesUpload}> 
+            Upload Gardens
           </Button> 
+
+          <MDBFile style={{marginBottom:20}} label='Upload Jogging Track File' id='joggingTrackFile' />
+          
+          <Button type='submit' className='mb-4' block onClick={handleJoggingTrackFilesUpload}>
+            Upload Jogging Tracks
+          </Button> 
+          
         </form>
         </div>
         </div>
