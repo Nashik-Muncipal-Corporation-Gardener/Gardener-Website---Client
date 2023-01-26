84726e7dacc89fd5ab5949e0fd2901c9a37520bf
ShivamVerma380 <shivam.r.verma@seagate.com>
2023-01-26 11:02:45 +0530

    Track grievance page integrated from backend apart from discussion thread
    Signed-off-by: ShivamVerma380 <shivam.r.verma@seagate.com>

--- gardener/src/Components/Grievance/Grievancedetails.js
+++ gardener/src/Components/Grievance/Grievancedetails.js
@@ -1,62 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "bootstrap/dist/css/bootstrap.css";
-import {Table, Button} from 'react-bootstrap';
-function Grievancedetails() {
+import { Table, Button } from 'react-bootstrap';
+import axios from 'axios';
+import url from '../../Uri';
+function Grievancedetails({ id }) {
+
+  console.log("Id", id);
+
+  const [grievanceForm,setGrievanceForm] = useState([]);
+  const [isGrievanceFormFetched,setIsGrievanceFormFetched] = useState(false);
+
+  function fetchGrievance() {
+    axios.get(url + "/grievance-form/" + id).then(function (response) {
+      if (response.status == 200) {
+        console.log(response.data)
+        setGrievanceForm(response.data)
+        setIsGrievanceFormFetched(true)
+      } else {
+        alert(response.data.message)
+      }
+    }).catch(function (error) {
+      console.log(error)
+      alert(error.response.data.message)
+    })
+  }
+
+
+  useEffect(()=>{
+    if(!isGrievanceFormFetched){
+      fetchGrievance()
+    }
+  })
+
   return (
     <div>
-       <center> <h5>Greivance Details</h5></center>
-        <Table striped bordered hover>
-      <tbody>
-        <tr>
-          <td>Complaint ID: </td>
-          <td>Complaint Date: </td>
-        </tr>
-        <tr>
-          <td>Department: </td>
-          <td>Sub Department: </td>
-        </tr>
-        <tr>
-          <td>Complaint Sub Type: </td>
-          <td>Mode of Complaint: </td>
-        </tr>
-        <tr>
-          <td>System Registered Date: </td>
-          <td>Status: </td>
-        </tr>
-        <tr>
-          <td>Division: </td>
-          <td>Prabhag: </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>Landmark: </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>Street Address: </td>
-        </tr>
-        <tr>
-          <td>Mobile: </td>
-          <td>Email: </td>
-        </tr>
-        <tr>
-          <td colSpan={2}>Image: </td>
-        </tr>
-      </tbody>
-    </Table>
-
-    <center><h5>Resolution Details</h5></center>
-        <Table striped bordered hover>
-      <tbody>
-        <tr>
-          <td>#</td>
-          <td>Start Day</td>
-          <td>End Day</td>
-          <td>Status</td>
-          <td>Reply by</td>
-          <td>Reply</td>
-        </tr>
+      <center> <h5>Greivance Details</h5></center>
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <td>Complaint ID:{grievanceForm.grievanceId}</td>
+            <td>Complaint Date:{grievanceForm.createdAt} </td>
+          </tr>
+          <tr>
+            <td>Department:{grievanceForm.department}</td>
+          </tr>
+          <tr>
+            <td>Complaint Type:{grievanceForm.grievanceType} </td>
+            <td>Mode of Complaint:Online</td>
+          </tr>
+          <tr>
+            <td>System Registered Date:{grievanceForm.createdAt}</td>
+            <td>Status:{grievanceForm.status}</td>
+          </tr>
+          <tr>
+            <td>Division:{grievanceForm.division}</td>
+            <td>Prabhag:{grievanceForm.prabhag}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Landmark:{grievanceForm.landmark}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Street Address:{grievanceForm.address}</td>
+          </tr>
+          <tr>
+            <td colSpan={2}>Image:View</td>
+          </tr>
         </tbody>
-        </Table>
-    <Button>Post your Reply</Button>
+      </Table>
+
+      <center><h5>Resolution Details</h5></center>
+      <Table striped bordered hover>
+        <tbody>
+          <tr>
+            <td>#</td>
+            <td>Start Day</td>
+            <td>End Day</td>
+            <td>Status</td>
+            <td>Reply by</td>
+            <td>Reply</td>
+          </tr>
+        </tbody>
+      </Table>
+      <Button>Post your Reply</Button>
     </div>
   )
 }
--- gardener/src/Components/Grievance/Trackgrievance.js
+++ gardener/src/Components/Grievance/Trackgrievance.js
@@ -5,31 +5,46 @@ import { MDBSelect, MDBInput, MDBCol, MDBRow, MDBFile, MDBBtn } from 'mdb-react-
 import { Row, Dropdown, DropdownButton, Col, Container, Button, Card, ListGroup, Modal, Table } from 'react-bootstrap'
 import '../../css/grievances.css'
 function Trackgrievance() {
+
   const [show, setShow] = useState(false);
   const [fullscreen, setFullscreen] = useState(true);
+
+  const [grievanceId, setGrievanceId] = useState('');
+
+
   function handleShow(breakpoint) {
+    if(grievanceId===""|| grievanceId===null){
+      alert("Please enter Grievance ID")
+      return;
+    }
     setFullscreen(breakpoint);
     setShow(true);
   }
+
+
+  function handleGrievanceId(e) {
+    setGrievanceId(e.target.value)
+  }
+
   return (
     <div className='registergrievance'>
       <Card style={{ width: '80%' }}>
         <ListGroup variant="flush">
           <ListGroup.Item style={{ backgroundColor: '#207fb0', color: 'aliceblue' }}>Track Grievance</ListGroup.Item>
           <ListGroup.Item style={{ padding: '5%' }} >
-            <MDBRow><MDBInput wrapperClass='mb-4' placeholder='Grievance ID' id='form1' type='number' /></MDBRow>
+            <MDBRow><MDBInput wrapperClass='mb-4' placeholder='Grievance ID' id='form1' type='number' onChange={handleGrievanceId} /></MDBRow>
             <MDBRow> <center><Button className='registergrievancebtn' onClick={() => handleShow(true)}>Track</Button>
             </center></MDBRow>
           </ListGroup.Item>
         </ListGroup>
       </Card>
-
+      
       <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
         <Modal.Header closeButton>
         </Modal.Header>
         <Modal.Body>
           <Container>
-            <Grievancedetails />
+            <Grievancedetails id={grievanceId} />
           </Container>
         </Modal.Body>
 
